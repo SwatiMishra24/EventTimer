1eff6aa16aa68b9b937ba04c8f8ad2e50ef491ae
swati <a_swati.mishra@africa.airtel.com>
2020-06-04 01:35:25 +0530

    initial commit

---
+++ Event Timer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Event Timer
+//
+//  Created by 2393260 on 6/4/20.
+//  Copyright © 2020 2393260. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
